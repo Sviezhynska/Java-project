0806b6d1d86991991b0847a0218b3745daebd5a1
Marina <135515516+Sviezhynska@users.noreply.github.com>
2023-07-17 23:11:45 +0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,25 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) {
+        PasswordGenerator generator = new PasswordGenerator();
+
+        loadWordsFromFile(generator, "words-password.txt");
+
+        String password = generator.generatePassword(1);
+        System.out.println("Password: " + password);
+    }
+
+    public static void loadWordsFromFile(PasswordGenerator generator, String fileName) {
+        try (BufferedReader reader = new BufferedReader(new FileReader(fileName))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                generator.addWord(line.trim());
+            }
+        } catch (IOException e) {
+            System.out.println("Error while reading file: " + e.getMessage());
+        }
+    }
+}
